09681115eba315ca864e17edb2d3902ac458fab7
Dr.GHOST <phichitonline@gmail.com>
2023-06-26 21:06:06 +0700

    Update model

--- app/Models/smartuser.php
+++ app/Models/smartuser.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class smartuser extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'username',
+        'password',
+        'ecid',
+        'ofid',
+        'lineid',
+        'email',
+        'tel',
+        'isadmin',
+        'consent',
+        'pincode',
+    ];
 }
