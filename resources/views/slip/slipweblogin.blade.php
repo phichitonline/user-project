bca374629a34c7ee11c8a0d9eaa04c2e4d1a7431
Dr.GHOST <phichitonline@gmail.com>
2023-08-06 21:08:09 +0700

    slip

--- resources/views/slip/slipweblogin.blade.php
+++ resources/views/slip/slipweblogin.blade.php
@@ -16,22 +16,24 @@
                 @csrf
                 @method('post')
 
-            <div class="input-style has-icon input-style-1 input-required pb-1">
-                <i class="input-icon fa fa-user color-theme"></i>
-                <span>เลขบัตรประชาชน</span>
-                <em>(required)</em>
-                <input name="cid" type="text" placeholder="เลขบัตรประชาชน">
-            </div>
-            <div class="input-style has-icon input-style-1 input-required pb-1">
-                <i class="input-icon fa fa-lock color-theme"></i>
-                <span>วันเดือนปีเกิด</span>
-                <em>(required)</em>
-                <input name="password" type="text" placeholder="ววดดปปปป (ปี พ.ศ.) 31122530">
-            </div>
+                <div class="input-style has-icon input-style-1 input-required pb-1">
+                    <i class="input-icon fa fa-user color-theme"></i>
+                    <span>เลขบัตรประชาชน</span>
+                    <em>(required)</em>
+                    <input name="cid" type="number" placeholder="เลขบัตรประชาชน">
+                </div>
+                <div class="input-style has-icon input-style-1 input-required pb-1">
+                    <i class="input-icon fa fa-lock color-theme"></i>
+                    <span>วันเดือนปีเกิด</span>
+                    <em>(required)</em>
+                    <input name="password" type="number" placeholder="ววดดปปปป (ปี พ.ศ.) 31122530">
+                </div>
 
-            <button type="submit" class="btn scale-box btn-m mt-3 btn-center-l rounded-l shadow-xl bg-green1-dark font-800 text-white  text-uppercase">Login</button>
+                <button type="submit" class="btn scale-box btn-m mt-2 mb-4 btn-center-l rounded-l shadow-xl bg-green1-dark font-800 text-white  text-uppercase">Login</button>
 
-            {{-- <a href="#" class="btn btn-m mt-2 mb-4 btn-full bg-green1-dark text-uppercase font-900">Login</a> --}}
+            </form>
+
+            <div class="clear"><br></div>
 
         </div>
 
