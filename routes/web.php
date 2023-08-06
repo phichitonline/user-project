e67fd33f5232cf15c16341415caa68ca21b21e5d
Dr.GHOST <phichitonline@gmail.com>
2023-08-06 20:36:41 +0700

    slip web

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,6 @@ Route::get('/main', [MainController::class, 'index'])->name('main');
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::get('/slip', [SlipController::class, 'index'])->name('slip');
 Route::get('/slipweb', [SlipController::class, 'slipweb'])->name('slipweb');
+Route::get('/slipweblogin', [SlipController::class, 'slipweblogin'])->name('slipweblogin');
 Route::get('/expenses', [ExpensesController::class, 'index'])->name('expenses');
 Route::get('/timestamp', [TimestampController::class, 'index'])->name('timestamp');
