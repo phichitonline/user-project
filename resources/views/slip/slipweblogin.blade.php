88046399a0b6b7ec73b213c0229ff0cc9bfd1e5e
Dr.GHOST <phichitonline@gmail.com>
2023-08-06 20:42:07 +0700

    slip update

--- resources/views/slip/slipweblogin.blade.php
+++ resources/views/slip/slipweblogin.blade.php
@@ -14,15 +14,15 @@
 
             <div class="input-style has-icon input-style-1 input-required pb-1">
                 <i class="input-icon fa fa-user color-theme"></i>
-                <span>Username</span>
+                <span>เลขบัตรประชาชน</span>
                 <em>(required)</em>
-                <input type="name" placeholder="Username">
+                <input name="cid" type="text" placeholder="เลขบัตรประชาชน">
             </div>
             <div class="input-style has-icon input-style-1 input-required pb-1">
                 <i class="input-icon fa fa-lock color-theme"></i>
-                <span>Password</span>
+                <span>วันเดือนปีเกิด</span>
                 <em>(required)</em>
-                <input type="name" placeholder="Password">
+                <input name="birthday" type="text" placeholder="วันเดือนปีเกิด">
             </div>
 
             <a href="#" class="btn btn-m mt-2 mb-4 btn-full bg-green1-dark text-uppercase font-900">Login</a>
